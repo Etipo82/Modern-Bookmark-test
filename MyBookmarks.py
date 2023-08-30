db94f9427d7c2d42f7f3cb0aaa81c18cafa3503b
Terence Abiama <49165098+Etipo82@users.noreply.github.com>
2023-08-30 17:09:24 -0400

    Add files via upload

---
+++ MyBookmarks.py
@@ -0,0 +1,35 @@
+import sqlite3
+import customtkinter
+import tkinter as tk
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    print("test")
+
+# Connect to the database (creates a new database if it doesn't exist)
+#conn = sqlite3.connect("myfavorites.db")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=20, fill='both', expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text='Username')
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text='Password', show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remeber Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
