e04b0f70f02ba2ddca8247535a316a88f4677beb
Terence Abiama <49165098+Etipo82@users.noreply.github.com>
2023-08-31 08:56:05 -0400

    Created classes and made functional
    Working with OOP, created classes simple design and functionality.

--- MyBookmarks.py
+++ MyBookmarks.py
@@ -1,35 +1,102 @@
 import sqlite3
 import customtkinter
 import tkinter as tk
+import webbrowser
 
-customtkinter.set_appearance_mode("system")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("500x350")
+conn = sqlite3.connect("bookmarks.db")
 
-def login():
-    print("test")
+# Create a bookmarks table
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS bookmarks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        url TEXT NOT NULL,
+        tags TEXT
+    )
+""")
 
-# Connect to the database (creates a new database if it doesn't exist)
-#conn = sqlite3.connect("myfavorites.db")
+conn.commit()
+conn.close()
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=20, fill='both', expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System")
-label.pack(pady=12, padx=10)
+class MyEntryFrame(customtkinter.CTkFrame):
+    def __init__(self, master, submit_callback):
+        super().__init__(master)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text='Username')
-entry1.pack(pady=12, padx=10)
+        self.entry_title = customtkinter.CTkEntry(self, placeholder_text="Site Name")
+        self.entry_title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.entry_url = customtkinter.CTkEntry(self, placeholder_text="URL")
+        self.entry_url.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.entry_tags = customtkinter.CTkEntry(self, placeholder_text="Tags")
+        self.entry_tags.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        
+        self.submit_button = customtkinter.CTkButton(self, text="submit", command=submit_callback)
+        self.submit_button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text='Password', show="*")
-entry2.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Login", command=login)
-button.pack(pady=12, padx=10)
+class MySearchBox(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, search_callback):
+        super().__init__(master)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Remeber Me")
-checkbox.pack(pady=12, padx=10)
+        self.search = customtkinter.CTkEntry(self, placeholder_text="Search Database")
+        self.search.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="e")
+        
+        self.search_button = customtkinter.CTkButton(self, text="search", command=search_callback)
+        self.search_button.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+        
 
-root.mainloop()
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Bookmark DB")
+        self.geometry("700x550")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.entry_frame = MyEntryFrame(self, self.submit_callback)
+        self.entry_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.search_frame = MySearchBox(self, self.search_callback)
+        self.search_frame.grid(row=0, column=3, padx=10, pady=(10, 0), sticky="nse")
+
+    def submit_callback(self):
+        title = self.entry_frame.entry_title.get()
+        url = self.entry_frame.entry_url.get()
+        tags = self.entry_frame.entry_tags.get().split(",")  # Split the input into a list of tags
+
+        conn = sqlite3.connect("bookmarks.db")
+        tags_str = ",".join(tags)
+        conn.execute("INSERT INTO bookmarks (title, url, tags) VALUES (?, ?, ?)", (title, url, tags_str))
+        conn.commit()
+        conn.close()
+
+        self.entry_frame.entry_title.delete(0, tk.END)
+        self.entry_frame.entry_url.delete(0, tk.END)
+        self.entry_frame.entry_tags.delete(0, tk.END)
+
+    def search_callback(self):
+        tags = self.search_frame.search.get()
+
+        conn = sqlite3.connect("bookmarks.db")
+        cursor = conn.execute("SELECT title, url, tags, id FROM bookmarks WHERE tags LIKE ?", ('%' + tags + '%',))
+
+        result = cursor.fetchall()
+
+        for row in result:
+            title, url, _, _ = row
+            button = customtkinter.CTkButton(self, text=title, command=lambda u=url: self.open_url(u))
+            button.grid(column=2, row=result.index(row), padx=10, pady=10, sticky="ew")
+
+        conn.close()
+
+    def open_url(self, url):
+        webbrowser.open(url)
+
+app = App()
+app.mainloop()
